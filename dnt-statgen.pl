7dcafd4566c5063d9ed73c8010622964eba3b8d7
Borek Lupomesky <borek@lupomesky.cz>
2017-09-20 10:00:41 +0200

    Add basic xlogfile parsing

--- dnt-statgen.pl
+++ dnt-statgen.pl
@@ -48,6 +48,42 @@ BEGIN
 
 
 #============================================================================
+#=== functions ==============================================================
+#============================================================================
+
+#============================================================================
+# Split a line along field separator, parse it into hash and return it as
+# a hashref.
+#============================================================================
+
+sub parse_log
+{
+  my $l = shift;
+  my %l;
+  my (@a1, @a2, $a0);
+
+  #--- there are two field separators in use: comma and horizontal tab;
+  #--- we use simple heuristics to find out the one that is used for given
+  #--- xlogfile row
+
+  @a1 = split(/:/, $l);
+  @a2 = split(/\t/, $l);
+  $a0 = scalar(@a1) > scalar(@a2) ? \@a1 : \@a2;
+
+  #--- split keys and values
+
+  for my $field (@$a0) {
+    my ($key, $val) = split(/=/, $field);
+    $l{$key} = $val;
+  }
+
+  #--- finish returning hashref
+
+  return \%l
+}
+
+
+#============================================================================
 #===================  _  ====================================================
 #===  _ __ ___   __ _(_)_ __  ===============================================
 #=== | '_ ` _ \ / _` | | '_ \  ==============================================
@@ -68,6 +104,19 @@ if(-f $lockfile) {
 open(F, '>', $lockfile) || die "Failed to create lockfile $lockfile";
 print F $$, "\n";
 close(F);
+
+#--- read the xlogfile
+
+my $cnt = 0;
+open(my $xlog, '<', $cfg->{'xlogfile'}) or die "Could not open the xlogfile";
+while(my $l = <$xlog>) {
+  chomp($l);
+  my $xrow = parse_log($l);
+  $cnt++;
+}
+close($xlog);
+
+print "Lines read: ", $cnt, "\n";
 
 #--- release lock file
 
